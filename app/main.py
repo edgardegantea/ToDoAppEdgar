068a342078cbbd5b2263df12dd73375bff79fcfc
edgar.degante.a@gmail.com <edgar.degante.a@gmail.com>
2025-09-19 17:57:23 -0600

    [Add] Agregué la función para guardar una nueva tarea y el formulario, y mostrar el formulario.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,38 @@
 import streamlit as st
+from utils.utils import get_tasks, save_tasks, Task
 
-st.title("Mi primera ventana")
+
+if "tasks" not in st.session_state:
+    st.session_state["tasks"] = get_tasks()
+
+
+
+def show_add_form():
+    with st.expander("Agregar tarea", expanded=True):
+        title = st.text_input("Título: ")
+        description = st.text_area("Descripción: ")
+
+        if st.button("Agregar"):
+            add_task(title, description)
+
+
+def add_task(title:str, description:str):
+    task = Task(
+        id=len(st.session_state.tasks) + 1,
+        title=title,
+        description=description
+    )
+
+    st.session_state.tasks.append(task.model_dump())
+    save_tasks(st.session_state.tasks)
+    
+
+
+def main():
+    st.header("To Do App")
+    show_add_form()
+    # print(tasks)
+
+if __name__ == "__main__":
+    main()
+
